60a9d50ad8eb9e9fee40f1eb8e4fdddd719ff9d7
Graham Reynolds <insidelane@gmail.com>
2025-11-07 09:38:26 +1000

    Fix tailwind and point to correct SVG

--- resources/views/widgets/cache_controller.blade.php
+++ resources/views/widgets/cache_controller.blade.php
@@ -2,48 +2,48 @@
     <header class="flex items-center justify-between p-2 border-b">
         <h2 class="flex items-center">
             <div class="w-6 h-6 mr-1 text-grey-80">
-                @cp_svg('content-writing')
+                @cp_svg('icons/light/content-writing')
             </div>
             <span>{{ __('Cache Manager') }}</span>
         </h2>
     </header>
-    <div class="flex flex-wrap px-2">
-        <div class="w-1/2 pr-1 py-2">
+    <div class="flex flex-wrap">
+        <div class="w-1/2 p-2">
             <h3 class="font-bold mb-1">{{ __('Content Stache') }}</h3>
             <form method="POST" action="{{ cp_route('utilities.cache.clear', 'stache') }}">
                 @csrf
-                <button class="btn w-full">{{ __('Clear') }}</button>
+                <button type="button" class="btn w-full">{{ __('Clear') }}</button>
             </form>
         </div>
-        <div class="w-1/2 pl-1 py-2">
+        <div class="w-1/2 p-2">
             <h3 class="font-bold mb-1">{{ __('Static Page Cache') }}</h3>
             @if (config('statamic.static_caching.strategy'))
                 <form method="POST" action="{{ cp_route('utilities.cache.clear', 'static') }}">
                     @csrf
-                    <button class="btn w-full">{{ __('Clear') }}</button>
+                    <button type="button" class="btn w-full">{{ __('Clear') }}</button>
                 </form>
             @else
                 disabled
             @endif
         </div>
-        <div class="w-1/2 pr-1 pb-2">
+        <div class="w-1/2 p-2">
             <h3 class="font-bold mb-1">{{ __('Application Cache') }}</h3>
             <form method="POST" action="{{ cp_route('utilities.cache.clear', 'application') }}">
                 @csrf
-                <button class="btn w-full">{{ __('Clear') }}</button>
+                <button type="button" class="btn w-full">{{ __('Clear') }}</button>
             </form>
         </div>
-        <div class="w-1/2 pl-1 pb-2">
+        <div class="w-1/2 p-2">
             <h3 class="font-bold mb-1">{{ __('Image Cache') }}</h3>
             <form method="POST" action="{{ cp_route('utilities.cache.clear', 'image') }}">
                 @csrf
-                <button class="btn w-full">{{ __('Clear') }}</button>
+                <button type="button" class="btn w-full">{{ __('Clear') }}</button>
             </form>
         </div>
-        <div class="w-full py-2 border-t">
+        <div class="w-full p-2 border-t">
             <form method="POST" action="{{ cp_route('utilities.cache.clear', 'all') }}">
                 @csrf
-                <button class="btn-primary w-full">{{ __('Clear All') }}</button>
+                <button type="button" class="btn-primary w-full">{{ __('Clear All') }}</button>
             </form>
         </div>
     </div>
